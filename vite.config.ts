573bf605deec6d7f35a1164c9e88a21cab91c9d5
JC、玄小张 <1216301160@qq.com>
2026-04-25 03:33:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -217,7 +217,7 @@ export default defineConfig({
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "/test-monorepo/dist/"),
+    outDir: path.resolve(import.meta.dirname, "./dist/"),
     emptyOutDir: true,
   },
   server: {
