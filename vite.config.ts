005fcee0fc054e8a36f6b7be6099465522e1fa15
1216301160 <1216301160@qq.com>
2026-04-25 03:16:34 +0800

    feat: path

--- vite.config.ts
+++ vite.config.ts
@@ -217,7 +217,7 @@ export default defineConfig({
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "./"),
     emptyOutDir: true,
   },
   server: {
