4919d1f06c2f5ffcd2c99315ec8ea311b58e4eb3
JC、玄小张 <1216301160@qq.com>
2026-04-25 05:18:12 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -207,7 +207,7 @@ const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(
 
 export default defineConfig({
   plugins,
-  // base: '/test-monorepo/',
+  base: '/test-monorepo/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
